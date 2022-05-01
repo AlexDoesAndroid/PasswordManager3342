6ac31836686ffe05e7b40a329a6b33f2ae612f9b
SeanPNolan <90008006+SeanPNolan@users.noreply.github.com>
2022-05-01 18:55:27 -0400

    comment

---
+++ PasswordManager3342/php/managerNewPassword.php
@@ -0,0 +1,17 @@
+﻿<?php
+	session_start();
+	require_once("config.php");
+	
+	// Connect DB
+	$con = mysqli_connect(SERVER, USER, PASSWORD, DATABASE);
+	if (!$con) {
+		// print "Database connection failed: ".mysqli_error($con);
+		$_SESSION["Message"] = "Database connection failed: ".mysqli_error($con);
+		$_SESSION["RegState"] = 1;
+		echo json_encode($_SESSION);
+		exit();
+	}
+
+	echo json_encode($_SESSION);
+	exit();
+?>
