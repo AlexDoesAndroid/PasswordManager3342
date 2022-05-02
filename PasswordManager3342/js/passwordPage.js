61dc7edac08746cc3242d23954b8566cd84578ec
SeanPNolan <90008006+SeanPNolan@users.noreply.github.com>
2022-05-02 15:51:31 -0400

    "managerDeletePassword", "managerEditPassword", and "managerNewPassword"

--- PasswordManager3342/js/passwordPage.js
+++ PasswordManager3342/js/passwordPage.js
@@ -65,3 +65,37 @@ $("#newPasswordBtn").click(function (e) {
     });
     return;
 });
+
+//New Password Btn
+$("#deleteBtn").click(function (e) {
+    event.preventDefault(e);
+    // Get form data items
+    var formData = {
+        'PreviousPassword': $('input[id=validationPreviousPassword]').val(),
+        'NewPassword': $('input[id=validationNewPassword]').val(),
+        'ServiceName': $('input[id=validationName]').val(),
+        'UserName': $('input[id=validationUserName]').val(),
+        'WebsiteURL': $('input[id=validationURL]').val(),
+        'Catagory': $('input[id=validationCatagory]').val()
+    };
+
+    alert("register Ajax. PreviousPassword[" + $('input[id=validationPreviousPassword]').val() + "]");
+    alert("register Ajax. NewPassword[" + $('input[id=validationNewPassword]').val() + "]");
+    alert("register Ajax. NewName[" + $('input[id=validationName]').val() + "]");
+    alert("register Ajax. UserName[" + $('input[id=validationUserName]').val() + "]");
+    alert("register Ajax. WebsiteURL[" + $('input[id=validationURL]').val() + "]");
+    alert("register Ajax. Catagory[" + $('input[id=validationCatagory]').val() + "]");
+
+    $.ajax({
+        type: 'DELETE',
+        url: 'php/managerDeletPassword.php',
+        async: true,
+        dataType: 'json',
+        encode: true
+    }).always(function (data) {
+        console.log(data);
+        return;
+    });
+    return;
+});
+
